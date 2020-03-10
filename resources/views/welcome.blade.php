38d8b10484e6b1d146b58ccebbd6d08dd9339be6
juanfug <54547859+juanfug@users.noreply.github.com>
2020-03-10 16:10:33 -0500

    commit de portada de inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,14 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        
         <title>Pensionate Colombia</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link rel="icon" href="img/pensionate (1).ico">
+        <link rel="stylesheet" href="css/main.css">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,800&display=swap" rel="stylesheet">
 
         <!-- Styles -->
         <style>
@@ -68,14 +70,17 @@
     </head>
     <body>
         <header>
+
             <div class="conteiner-header">
                 <div class="logo-title">
-                    <img src="" alt="">
+                    <img src="img/logo.PNG" alt="">
+                    <h5><a href="http://127.0.0.1:8000/">Pensionate Colombia</a></h5>
+                    
                 </div>
+
             </div>
-        </header>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
+
+                        @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
@@ -85,26 +90,42 @@
                     @endauth
                 </div>
             @endif
-
+        </header>
+            <div class="container-portadas">
+                <div class="capa-gradiente"></div>
+                <div class="container-detalles"></div>
+                    <div class="detalles">
+                        <h1>Pensiónate Colombia</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa autem, maiores fugiat magni, fuga corrupti iste numquam nulla suscipit id inventore. Delectus, odit, voluptatum! Perspiciatis debitis, voluptatem vitae obcaecati perferendis.</p>
+                        <button>Conocenos y a Nuestro Equipo de Trabajo</button> 
+                    </div>
+            </div>
+        <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    Pensiónate Colombia
-                    <style>
-                        div {
-                            background: ;
-                            }
-                    </style>
+                    Pensiónate Colombia     
                 </div>
 
                 <div class="links">
-                    <a href="">Pensiones</a>
                     <a href="">Apartamentos</a>
                     <a href="">Casas</a>
-                    <a href="">Vacaciones y festivos</a>
                     <a href="">Inmuebles en venta</a>
+                    <a href="">Pensiones</a>
+                    <a href="">Vacaciones y festivos</a>
                 </div>
             </div>
         </div>
+        <main>
+            <article>
+                <h1>
+                    Conoce como trabajamos y ejemplos de nuestros servicios
+                </h1>
+                <hr>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat dolor provident ut hic, porro tenetur quos tempore ullam harum ea!</p>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae natus mollitia sed quis, vel numquam, dolore ut dignissimos consectetur eaque quia totam. Maiores nobis tempore atque? Ducimus voluptas corporis pariatur nobis sed sequi, excepturi veritatis aliquam ad! Facere animi earum mollitia, non dolor aliquam necessitatibus beatae eaque, delectus ducimus totam.</p>
+            </article>
+        </main>
     </body>
 </html>
-https://www.youtube.com/watch?v=MNr5QqaDg4I
